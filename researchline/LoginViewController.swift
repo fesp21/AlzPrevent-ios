151e7148699139c08bf5a4e98257b72736ced3ee
riverleo <iam.riverleo@gmail.com>
2015-11-19 01:12:14 +0900

    make profile layout

--- researchline/LoginViewController.swift
+++ researchline/LoginViewController.swift
@@ -9,10 +9,17 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-
-    navigationController?.navigationBarHidden = false
-  }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationController?.navigationBarHidden = false
+    }
+    
+    @IBAction func touchUpInsideDoneBarButtonItem(sender: UIBarButtonItem) {
+        let storyboard = UIStoryboard(name: "TabBar", bundle: nil)
+        let controller = storyboard.instantiateInitialViewController()!
+        
+        presentViewController(controller, animated: true, completion: nil)
+    }
 }
